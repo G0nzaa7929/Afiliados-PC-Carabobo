d5f121ac771e6b6089b0ef9149ca717df593c43a
G0nzaa7929 <tspablo723@gmail.com>
2026-09-05 15:25:43 -0500

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+ // CREDENCIALES DE TU PROYECTO
+  var firebaseConfig = {
+    apiKey: "APIKEY",
+    authDomain: "sistema-pc-carabobo.firebaseapp.com",
+    databaseURL: "https://sistema-pc-carabobo-default-rtdb.firebaseio.com/",
+    projectId: "sistema-pc-carabobo",
+    storageBucket: "sistema-pc-carabobo.appspot.com",
+    messagingSenderId: "559128386152",
+    appId: "1:559128386152:web:d23826e19988b9e641f9dd"
+  };
